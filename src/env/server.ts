f205a7e66b9a397bc2eadfce222e9cd010c66113
shishir435 <shishirchaurasiya435@gmail.com>
2024-09-27 07:25:51 +0530

    chore: add t3env to parse env

---
+++ src/env/server.ts
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { ZodError, z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["production", "development"]),
+  },
+  onValidationError: (error: ZodError) => {
+    console.error(
+      "❌ Invalid environment variables:",
+      error.flatten().fieldErrors
+    );
+    process.exit(1);
+  },
+  // eslint-disable-next-line n/no-process-env
+  experimental__runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+});
